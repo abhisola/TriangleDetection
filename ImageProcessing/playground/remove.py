86643290405dfd68ec916c4dd507b7accba06506
azizahtas <azizahtas@gmail.com>
2018-08-14 00:16:14 +0530

    testing Loging Functionality

--- ImageProcessing/playground/remove.py
+++ ImageProcessing/playground/remove.py
@@ -1,2 +1,10 @@
 import shutil
-shutil.rmtree('/temp/s3')
+#shutil.rmtree('/temp/s3')
+
+def create_log(date):
+    path = "./logs/"+date+".txt"
+    f = open(path, "w+")
+    f.write('''Helllo Friends Chai Peelo''')
+    f.close()
+
+create_log("2018-06-12")
--- ImageProcessing/process.py
+++ ImageProcessing/process.py
@@ -2,11 +2,32 @@ import subprocess
 import getopt
 import sys
 from datetime import date, timedelta
-
+import requests
+import psycopg2
+import json
+settings = {}
 RACKS = ('001', '002')
-log = ''''''
+
 def main(argv):
-    global log
+    global racks
+    with open('settings.json') as jsonData:
+      settings = json.load(jsonData)
+      print(settings)
+      jsonData.close()
+
+    hostname = settings['postgres']['hostname']
+    username = settings['postgres']['username']
+    password = settings['postgres']['password']
+    database = settings['postgres']['database']
+
+    conn = psycopg2.connect(host=hostname, user=username, password=password, dbname=database)
+    cursor = conn.cursor()
+    cursor.execute("SELECT racknum FROM racks ORDER BY racknum ASC")
+    if cursor.rowcount > 0:
+        racks = cursor.fetchall()
+    cursor.close()
+    conn.close()
+
     # set default date to yesterday
     yesterday = date.today() - timedelta(1)
     subjectDate = yesterday.strftime('%Y-%m-%d')
@@ -16,7 +37,7 @@ def main(argv):
     subjectDay = dateParts[2]
     # parse commandline parameters
     try:
-        opts, args = getopt.getopt(argv, "d:")
+        opts, args = getopt.getopt(argv, "s:")
     except getopt.GetoptError:
         usage()
         sys.exit(2)
@@ -35,32 +56,21 @@ def main(argv):
                 print("Invalid date")
                 usage()
                 sys.exit(2)
-    log += 'Processing For Date '+subjectDate+'\n'
-    for rack in RACKS:
-        log += '**************\n'
-        log += 'Processing Rack '+rack+'\n'
+
+    for rack in racks:
         command = '''python processrackimages.py -r {0} -d {1}'''.format( rack, subjectDate)
         print('\n' + 'Processing Rack '+rack)
-        log += '---------\n'
         try:
             process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
-            log += process.stdout.read()
-            print(process.stdout.read())
             process.wait()
         except subprocess.TimeoutExpired:
             pass
 
-        log += '-------\\------\n'
 
 def usage():
     print("python custom_process.py -d YYYY-MM-DD")
     print("  -d YYYY-MM-DD : UTC date ex: 2016-11-28")
 
-def create_log(date):
-    path = "/logs/"+date+".txt"
-    f = open(path, "w+")
-    f.write(log)
-    f.close()
 
 # call main function
 if __name__ == "__main__":
